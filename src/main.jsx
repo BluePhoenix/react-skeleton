c8799faab41725d65fc812376c924d5bf21a72f2
Felix Barros <felix.barros@gmail.com>
2015-12-20 21:10:11 -0500

    Add component to main.jsx and some content to index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,7 @@
 // Main entry point for the application
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var List = require('./components/List.jsx');
+
+ReactDOM.render(<List />, document.getElementByID('ingredients'));
