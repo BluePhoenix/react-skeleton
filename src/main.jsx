8f2e67e9250b957623d0a764d8a86f24db290824
Felix Barros <felix.barros@gmail.com>
2015-12-22 13:07:36 -0500

    Remove test components

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,3 @@
 // Main entry point for the application
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var ListA = require('./components/List.jsx');
-
-ReactDOM.render(<ListA />, document.getElementById('ingredients'));
